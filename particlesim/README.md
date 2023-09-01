190b4090c86e1afb88ed605bf36d82598ee9a6d4
noahl25 <97693037+noahl25@users.noreply.github.com>
2023-08-31 22:03:21 -0700

    Delete particlesim directory

--- particlesim/README.md
+++
@@ -1,2 +0,0 @@
-this sucks big time so have low expectations
-you need sdl dll's in same folder as exe 
--- particlesim/src/Common.cpp
+++
@@ -1,5 +0,0 @@
-#include "Common.hpp"
-
-uint16_t SCREEN_WIDTH;
-uint16_t SCREEN_HEIGHT;
-uint8_t P_SIZE = 5;
